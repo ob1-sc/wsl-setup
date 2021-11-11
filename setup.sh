f96bdf6ba84e2628dc46d4d52a03950332022235
Simon O'Brien <simonobrien@vmware.com>
2021-11-11 20:26:02 +0000

    fixed ansible local user var

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ git clone https://github.com/ob1-sc/wsl-setup --recurse-submodules
 
 echo "Running setup playbook ..."
 sudo ansible-playbook \
-	--extra-vars "user=$(whoami) ansible_python_interpreter=/usr/bin/python3" \
+	--extra-vars "local_user=$(whoami) ansible_python_interpreter=/usr/bin/python3" \
 	--connection=local \
 	--inventory 127.0.0.1, \
 	./wsl-setup/local.yaml
