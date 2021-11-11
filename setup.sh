f1845bad23076815b90b6ec26c0980d51ea41703
Simon O'Brien <simonobrien@vmware.com>
2021-11-11 20:20:36 +0000

    fixed playbook path

--- setup.sh
+++ setup.sh
@@ -15,4 +15,4 @@ sudo ansible-playbook \
 	--extra-vars "user=$(whoami) ansible_python_interpreter=/usr/bin/python3" \
 	--connection=local \
 	--inventory 127.0.0.1, \
-	./wsl-setup/ansible-playbook/local.yml
+	./wsl-setup/local.yml
