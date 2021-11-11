b6f44edd239faa0ce79f41fd00e4ce2fb6906f1d
Simon O'Brien <simonobrien@vmware.com>
2021-11-11 20:15:57 +0000

    added non-interactive flags

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 
 echo "Installing ansible ..."
 sudo apt update
-sudo apt install software-properties-common python3-jmespath
+sudo apt install -y software-properties-common python3-jmespath
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt update
+sudo apt install -y ansible
 
 echo "Preparing setup playbook ..."
 git clone https://github.com/ob1-sc/wsl-setup --recurse-submodules
