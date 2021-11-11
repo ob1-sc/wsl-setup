8b91f9ae37fb4ba3997c1574c79eee34be22fb4f
Simon O'Brien <simonobrien@vmware.com>
2021-11-11 20:14:13 +0000

    added ansible install

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+echo "Installing ansible ..."
+sudo apt update
+sudo apt install software-properties-common python3-jmespath
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+echo "Preparing setup playbook ..."
 git clone https://github.com/ob1-sc/wsl-setup --recurse-submodules
 
+echo "Running setup playbook ..."
 sudo ansible-playbook \
 	--extra-vars "user=$(whoami) ansible_python_interpreter=/usr/bin/python3" \
 	--connection=local \
