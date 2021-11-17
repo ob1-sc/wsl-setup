9fd7f154b9f88b01b52b29c4c229290c88717893
Simon O'Brien <simon@the-obriens.net>
2021-11-17 16:54:35 +0000

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -7,12 +7,15 @@ sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt update
 sudo apt -y install ansible
 
+echo "Cleaning up existing setup ..."
+rm -rf ~/.wsl-setup
+
 echo "Preparing setup playbook ..."
-git clone https://github.com/ob1-sc/wsl-setup --recurse-submodules
+git clone https://github.com/ob1-sc/wsl-setup ~/.wsl-setup --recurse-submodules
 
 echo "Running setup playbook ..."
 sudo ansible-playbook \
 	--extra-vars "local_user=$(whoami) ansible_python_interpreter=/usr/bin/python3" \
 	--connection=local \
 	--inventory 127.0.0.1, \
-	./wsl-setup/local.yaml
+	./.wsl-setup/local.yaml
