d20bbf860f1a599f95995c0dbf35f6fdf93afefb
Simon O'Brien <simon@the-obriens.net>
2021-11-17 16:59:33 +0000

    Added cleanup for wsl-setup dir

--- setup.sh
+++ setup.sh
@@ -7,8 +7,10 @@ sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt update
 sudo apt -y install ansible
 
+WORKING_DIR=~/.wsl-setup
+
 echo "Cleaning up existing setup ..."
-rm -rf ~/.wsl-setup
+if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
 
 echo "Preparing setup playbook ..."
 git clone https://github.com/ob1-sc/wsl-setup ~/.wsl-setup --recurse-submodules
@@ -18,4 +20,4 @@ sudo ansible-playbook \
 	--extra-vars "local_user=$(whoami) ansible_python_interpreter=/usr/bin/python3" \
 	--connection=local \
 	--inventory 127.0.0.1, \
-	./.wsl-setup/local.yaml
+	$WORKING_DIR/local.yaml
